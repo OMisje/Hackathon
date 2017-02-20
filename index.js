e899f8fe5d4f71b05580b86955ec11515f9f8826
Jarle Kallevik <jka@statoil.com>
2017-02-20 14:21:03 +0100

    commit 3 with motion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // console.log("Hello world");
 
 var five = require("johnny-five");
+var snapshot = require("./snapshot.js");
 
 var board = new five.Board({
 	repl: false,
@@ -9,6 +10,7 @@ var board = new five.Board({
 
 board.on("ready", function() {
 	console.log("Board is ready");
+	var motion = new five.Motion(8);
 	var led = new five.Led(9);
 	var button = new five.Button({
 		pin: 7,
@@ -24,7 +26,33 @@ board.on("ready", function() {
 	button.on("press", function() {
 		led.toggle();
 		console.log("Temperature " + temp.celsius + "C");
+		snapshot.takeSnapshot(function callback(err) {
+			if (err) {
+				console.log('Unable to take camera snapshot ', err);
+			}
+		});
 	});
+	
+	motion.on("calibrated", function() {
+		console.log("Motion calibrated");
+	});
+	
+	motion.on("motionend", function() {
+		console.log("Motion ended");
+		led.off();
+	});
+	
+	motion.on("motionstart", function() {
+		console.log("Motion detected");
+		led.on();
+		
+		snapshot.takeSnapshot(function callback(err) {
+			if (err) {
+				console.log('Unable to take camera snapshot ', err);
+			}
+		});
+	});
+	
 	// button.on("release", function() {
 // 		led.toggle();
 //	});
